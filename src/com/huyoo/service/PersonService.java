7f7823afbb01ea08d6cee55cd4370f7d5316780d
xufei <xunminian@163.com>
2015-03-02 18:43:18 +0800

    add package:com/huyoo/service

---
+++ src/com/huyoo/service/PersonService.java
@@ -0,0 +1,19 @@
+package com.huyoo.service;
+
+import java.util.List;
+
+import com.huyoo.entity.EPerson;
+
+/**
+ * 
+ * @author xufei 20150302
+ *
+ */
+public class PersonService {
+	
+	public List<EPerson> getPersons()
+	{
+		return null;
+	}
+	
+}
