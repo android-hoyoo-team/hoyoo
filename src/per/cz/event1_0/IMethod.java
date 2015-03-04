f0e0dc09400cb3216b6bf81cb2ff866865af1b8f
huangjilong <huangjilong.g@gmail.com>
2015-03-04 18:13:31 +0800

    update

--- src/per/cz/event1_0/IMethod.java
+++ src/per/cz/event1_0/IMethod.java
@@ -1,6 +1,22 @@
 package per.cz.event1_0;
 
-public interface IMethod<T>
+import java.util.Date;
+
+public abstract class IMethod<T>
 {
-	 void excute(DEvent<T> event);
+	public abstract void excute(DEvent<T> event);
+	public long getID(){
+		 return new Date().getTime();
+	 }
+	@Override
+	public boolean equals(Object obj) {
+		if(obj==null)
+			return false;
+		if(this==obj)
+			return true;
+		if(this.getID()==((IMethod)obj).getID())
+			return true;
+		return super.equals(obj);
+	}
+	
 }
