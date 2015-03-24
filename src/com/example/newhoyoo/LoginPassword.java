8e87ea13634d77fd27b6daeb129f37e39d4c6439
huangjilong <huangjilong.g@gmail.com>
2015-03-25 01:47:51 +0800

    u

--- src/com/example/newhoyoo/LoginPassword.java
+++ src/com/example/newhoyoo/LoginPassword.java
@@ -40,14 +40,14 @@ public class LoginPassword extends Activity {
 		setContentView(R.layout.login);
 		this.aq=new AQuery(this);
 		this.aq.id(R.id.password_imageview).clicked(this, "login");
-		DispatchEvent.addEventListener("message", new IMethod<String>() {
-
-			@Override
-			public void excute(DEvent<String> event) {
-				// TODO Auto-generated method stub
-				Toast.makeText(getApplicationContext(), event.getTarget().toString(), Toast.LENGTH_LONG).show();
-			}
-		});
+//		DispatchEvent.addEventListener("message", new IMethod<String>() {
+//
+//			@Override
+//			public void excute(DEvent<String> event) {
+//				// TODO Auto-generated method stub
+//				Toast.makeText(getApplicationContext(), event.getTarget().toString(), Toast.LENGTH_LONG).show();
+//			}
+//		});
 	}
 	public void login(){
 		String password = this.aq.id(R.id.password_edittext).getText().toString().trim();
