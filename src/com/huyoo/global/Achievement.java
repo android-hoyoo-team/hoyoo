84ea7b1811fc73dd761f05b7520312538e2be1bf
huangjilong <huangjilong.g@gmail.com>
2015-03-25 01:29:25 +0800

    a

---
+++ src/com/huyoo/global/Achievement.java
@@ -0,0 +1,13 @@
+package com.huyoo.global;
+
+import per.cz.event1_0.DEvent;
+import per.cz.event1_0.DispatchEvent;
+
+public class Achievement {
+	public static void login(){
+		DispatchEvent.dispatchEvent(new DEvent("login", "message"));
+	}
+	public static void enterAchievement(){
+		DispatchEvent.dispatchEvent(new DEvent("enterAchievement", "message"));
+	}
+}
